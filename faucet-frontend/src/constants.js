27543364ff850e918f5f05a91476df0a3f268422
Anish Duwal <duwalanise@gmail.com>
2020-07-22 10:15:57 +0545

    remove txnExplorer constant

--- faucet-frontend/src/constants.js
+++ faucet-frontend/src/constants.js
@@ -1,5 +1,4 @@
 const baseUrl = process.env.REACT_APP_HOST_API || "";
-const txnExplorer = process.env.REACT_APP_TXN_EXPLORER || "";
 const reCaptchaKey = process.env.REACT_APP_RECAPTCHA_KEY || "";
 
-export { baseUrl, txnExplorer, reCaptchaKey };
+export { baseUrl, reCaptchaKey };
