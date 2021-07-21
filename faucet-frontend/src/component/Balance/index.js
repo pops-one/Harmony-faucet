02deb0fb7d254750dc606db8279c3675a84199c2
P-OPS <61007546+pops-one@users.noreply.github.com>
2021-07-21 17:47:42 +0400

    Update index.js

--- faucet-frontend/src/component/Balance/index.js
+++ faucet-frontend/src/component/Balance/index.js
@@ -5,7 +5,14 @@ import "./style.scss";
 const Balance = ({ balance }) => {
   return (
     <div className="balance-wrapper">
-      Current balance in this faucet is {balance} ONE.
+      Current balance in this faucet is {balance} ONE. <br/><br/>
+
+      This Faucet is intended for use by DApp Developers on Harmony Testnet. <br/> <br/>
+      We recommend that you <a href="https://staking.harmony.one">stake</a> a portion of the tokens received from the Faucet, so that you have a continuous source of Testnet tokens. <br/> <br/>
+      If you wish to receive additional tokens to run a validator on testnet, please drop a message <a href="https://t.me/POPS_Team_Validator">here.</a><br/> <br/>
+
+      Please return unused Testnet tokens back to the faucet.
+      
     </div>
   );
 };
